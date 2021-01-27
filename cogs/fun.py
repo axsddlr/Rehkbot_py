5412fc60436afc903c9c7b70be1f68c82334e272
Andre Saddler <dresadd09691@gmail.com>
2021-01-26 21:40:48 -0500

    cogs: games -> fun

--- cogs/fun.py
+++ cogs/fun.py
@@ -4,7 +4,7 @@ import time
 from discord.ext import commands
 
 
-class GamesCog(commands.Cog, name='GamesN'):
+class FunCog(commands.Cog, name='Games & Fun'):
     def __init__(self, bot):
         self.bot = bot
 
@@ -42,9 +42,29 @@ class GamesCog(commands.Cog, name='GamesN'):
         response = f"Question: {arg}\nAnswer: {random.choice(answers)}"
         await ctx.send(response)
 
+    @commands.command(aliases=['flip', 'coin'])
+    async def coinflip(self, ctx):
+        """ Coinflip! """
+        coinsides = ['Heads', 'Tails']
+        await ctx.send(f"**{ctx.author.name}** flipped a coin and got **{random.choice(coinsides)}**!")
+
+    @commands.command(aliases=['pfp'], name='Avatar', help='Get the avatar URL of the tagged user(s), or your own avatar')
+    async def avatar(self, ctx, user: discord.User = ''):
+        if not user:
+            user = ctx.author
+            uid = str(ctx.author.id)
+        else:
+            uid = str(user.id)
+        author = ctx.message.author
+        pfp = author.avatar_url
+        embed = discord.Embed()
+        embed.set_image(url=pfp)
+        embed.set_author(name=user.display_name, icon_url=user.avatar_url)
+        await ctx.send(embed=embed)
+
 # The setup fucntion below is neccesarry. Remember we give bot.add_cog() the name of the class in this case MembersCog.
 # When we load the cog, we use the name of the file.
 
 
 def setup(bot):
-    bot.add_cog(GamesCog(bot))
+    bot.add_cog(FunCog(bot))
