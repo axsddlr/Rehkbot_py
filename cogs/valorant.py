8e9691e801b25fd3f5d55f27408ad738f532cc4e
Andre Saddler <dresadd09691@gmail.com>
2021-02-03 21:14:32 -0500

    cogs - Valorant: allow user to check their own stats/rank

--- cogs/valorant.py
+++ cogs/valorant.py
@@ -1,29 +1,33 @@
 import discord
 import requests
 from datetime import datetime
+from urllib.parse import unquote
 from discord.ext import commands
 from discord.ext.commands.cooldowns import BucketType
 
 
 def getValorantGameUpdates():
     URL = "https://api.henrikdev.xyz/valorant/v1/website/en-us?filter=game_updates"
-
     response = requests.get(URL)
     return response.json()
 
 
-def getValorantStats():
-    URL = "https://api.henrikdev.xyz/valorant/v1/mmr/na/Rehkloos/001"
-
+def getValorantRank(Name, Tag):
+    URL = "https://api.henrikdev.xyz/valorant/v1/mmr/na/" + Name + "/" + Tag
     response = requests.get(URL)
     return response.json()
-    print(URL)
 
+
+def getValorantStats(Name, Tag):
+    URL = "https://api.henrikdev.xyz/valorant/v1/profile/" + Name + "/" + Tag
+    response = requests.get(URL)
+    return response.json()
 
 class Valorant(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    #  Valorant News
     @commands.command(name="Valorant Updates",
                       aliases=['valupdates'],
                       help='Valorant specific commands')
@@ -39,9 +43,6 @@ class Valorant(commands.Cog):
         banner = responseJSON['data'][0]['banner_url']
         title = responseJSON['data'][0]['title']
         url = responseJSON['data'][0]['url']
-        # utc_dt = datetime.strptime(responseJSON['data'][0]['date'], '%Y-%m-%dT%H:%M:%S.%fZ')
-
-        # await ctx.message.channel.send(banner)
 
         embed = discord.Embed(
             title=title,
@@ -49,7 +50,6 @@ class Valorant(commands.Cog):
             # crimson color code
             colour=(0xDC143C)
         )
-        # embed.set_thumbnail(url=ctx.bot.user.avatar_url)
         embed.set_image(url=banner)
 
         await ctx.send(embed=embed)
@@ -57,14 +57,54 @@ class Valorant(commands.Cog):
     @commands.command(name="Valorant Rank ",
                       aliases=['valrank'],
                       help='Valorant specific commands')
-    @commands.cooldown(2, 60, BucketType.user)
-    async def valstats(self, ctx):
+    # @commands.cooldown(2, 60, BucketType.user)
+    async def valrank(self, ctx, Name: str = "", Tag: str = ""):
 
-        responseJSON = getValorantStats()
-        rank = responseJSON['data']['currenttierpatched']
-        elo = responseJSON['data']['elo']
+        responseJSON = getValorantRank(Name, Tag)
 
-        await ctx.message.channel.send("Rank: " + str(rank) + " | " + "Elo: " + str(elo))
+        # Check if profile exists or is public
+        status = responseJSON['status']
+        if status == '200':
+            riotid = Name + "#" + Tag
+            rank = responseJSON['data']['currenttierpatched']
+            elo = responseJSON['data']['elo']
+
+            await ctx.message.reply("Rank: " + str(rank) + " | " + "Elo: " + str(elo))
+            #  If it does not exist or is private
+        elif status == '451':
+            message = responseJSON['message']
+            await ctx.message.reply(message)
+        elif status == '404':
+            message = responseJSON['message']
+            await ctx.message.reply(message)
+
+    @commands.command(name="Valorant Stats ",
+                      aliases=['valstats'],
+                      help='Valorant specific commands')
+    # @commands.cooldown(2, 60, BucketType.user)
+    async def valstats(self, ctx, Name: str = "", Tag: str = ""):
+
+            responseJSON = getValorantStats(Name, Tag)
+
+            # Check if profile exists or is public
+            status = responseJSON['status']
+            if status == '200':
+                riotid = Name + "#" + Tag
+                kills = responseJSON['stats']['kills']
+                deaths = responseJSON['stats']['deaths']
+                kdr = responseJSON['stats']['kdratio']
+                wins = responseJSON['stats']['wins']
+                winr = responseJSON['stats']['winpercentage']
+                TTP = responseJSON['stats']['playtime']['playtimepatched']
+
+                await ctx.message.reply("Total Time Played: " + str(TTP) + " | Wins: " + str(wins) + " | Win/Loss: " + str(winr) + " | Kills: " + str(kills) + " | KDR: " + str(kdr) + " | Deaths: " + str(deaths) + " (" + unquote(riotid) + ")")
+            #  If it does not exist or is private
+            elif status == '451':
+                message = responseJSON['message']
+                await ctx.message.reply(message)
+            elif status == '404':
+                message = responseJSON['message']
+                await ctx.message.reply(message)
 
 
 def setup(bot):
