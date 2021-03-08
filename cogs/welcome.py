cb41a5e69501fb2ea7073b6b2ba414005d4ffb37
Andre Saddler <dresadd09691@gmail.com>
2021-03-07 21:07:49 -0500

    add Welcome Cog

---
+++ cogs/welcome.py
@@ -0,0 +1,48 @@
+import discord
+import time
+import sys
+import psutil
+import os
+import datetime
+from discord.ext import commands
+from discord.ext.commands import Cog
+
+WELCOME_CHANNEL_ID = 362861909218689024
+LEAVE_CHANNEL_ID = 362861909218689024
+
+class Streaming(Cog, name='Streaming'):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @Cog.listener()
+    async def on_member_join(self, member):
+        try:
+            channel = bot.get_channel(WELCOME_CHANNEL_ID)
+            try:
+                embed = discord.Embed(colour=discord.Colour.green())
+                embed.set_author(name=member.name, icon_url=member.avatar_url)
+                embed.add_field(name="Welcome", value=f"**Hey,{member.mention}! Welcome to {member.guild.name}\nI hope you enjoy your stay here!\nThanks for joining**", inline=False)
+                embed.set_thumbnail(url=member.guild.icon_url)
+                await channel.send(embed=embed)
+            except Exception as e:
+                raise e
+        except Exception as e:
+            raise e
+
+    @Cog.listener()
+    async def on_member_remove(self, member):
+        try:
+            channel = bot.get_channel(LEAVE_CHANNEL_ID)
+            try:
+                embed = discord.Embed(colour=discord.Colour.red())
+                embed.set_author(name=member.name, icon_url=member.avatar_url)
+                embed.add_field(name="Good Bye", value=f"**{member.mention} just left us.**", inline=False)
+                embed.set_thumbnail(url=member.guild.icon_url)
+                await channel.send(embed=embed)
+            except Exception as e:
+                raise e
+        except Exception as e:
+            raise e
+
+def setup(bot):
+    	bot.add_cog(Streaming(bot))
