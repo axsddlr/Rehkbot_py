f5e8534ff600e7d90e223f9b9b7202817fdc2df7
Andre Saddler <dresadd09691@gmail.com>
2021-04-02 20:24:11 -0400

    allow links in 'valorant-news' and 'patch-notes'

--- cogs/linkchecker.py
+++ cogs/linkchecker.py
@@ -10,7 +10,7 @@ from discord.ext.commands import command, has_permissions, bot_has_permissions
 class Links(Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.links_allowed = (817593711054815252,512694755847831562,)
+        self.links_allowed = (817593711054815252,512694755847831562,512698020484087812,824453152526565427)
         self.url_regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
 
     @Cog.listener()
