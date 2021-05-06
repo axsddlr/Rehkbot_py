4d45be1def027cfdfb22ca154b833e91bd750693
Andre Saddler <dresadd09691@gmail.com>
2021-05-05 22:38:17 -0400

    game_updates: add TFT and fix up apis

--- cogs/game_updates.py
+++ cogs/game_updates.py
@@ -9,7 +9,7 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
 from discord.ext import commands
 from discord.ext.commands.cooldowns import BucketType
-from utils.discord_webhook import Webhook, Embed
+from utils.discord_webhook import Webhook, Embed, File
 from utils.functions import exists
 
 load_dotenv()
@@ -30,7 +30,7 @@ def getLOLGameUpdates():
 
 
 def getTFTGameUpdates():
-    URL = "https://api.rehkloos.com/tft/patch_notes"
+    URL = "https://api.rehkloos.com/tft/en-us/patch_notes"
     response = requests.get(URL)
     return response.json()
 
@@ -58,11 +58,11 @@ class Game_Updates(commands.Cog, name="Game Updates"):
         responseJSON = getValorantGameUpdates()
 
         # JSON Results Mapping
-        banner = responseJSON["data"]["segments"]["thumbnail_url"]
-        title = responseJSON["data"]["segments"]["title"]
-        description = responseJSON["data"]["segments"]["description"]
-        url = responseJSON["data"]["segments"]["url_path"]
-        external_link = responseJSON["data"]["segments"]["external_link"]
+        banner = responseJSON["data"]["segments"][0]["thumbnail"]
+        title = responseJSON["data"]["segments"][0]["title"]
+        description = responseJSON["data"]["segments"][0]["description"]
+        url = responseJSON["data"]["segments"][0]["url_path"]
+        external_link = responseJSON["data"]["segments"][0]["external_link"]
 
         if external_link == "":
             full_url = "https://playvalorant.com/en-us" + url
@@ -80,7 +80,7 @@ class Game_Updates(commands.Cog, name="Game Updates"):
         )
         data = json.load(f)
         res = updater(data, "", None)
-        check_file_json = res["data"]["segments"]["title"]
+        check_file_json = res["data"]["segments"][0]["title"]
 
         # compare title string from file to title string from api then overwrite file
         if check_file_json == title:
@@ -101,8 +101,13 @@ class Game_Updates(commands.Cog, name="Game Updates"):
             )
             embed.set_footer(text="Rehkbot")
             embed.set_image(url=banner)
+            file = File(
+                "./assets/images/valorant_sm.png",
+                name="valorant_sm.png",
+            )
+            embed.set_thumbnail(url="attachment://valorant_sm.png")
 
-            hook.send(embed=embed)
+            hook.send(embed=embed, file=file)
 
             f = open(saved_json, "w")
             print(json.dumps(responseJSON), file=f)
@@ -143,16 +148,77 @@ class Game_Updates(commands.Cog, name="Game Updates"):
             hook = Webhook(patches_webhook)
 
             embed = Embed(
-                title=title,
-                description=url,
+                title="League of Legends",
+                description=f"[{title}]({url})\n\n",
                 color=crimson,
                 timestamp="now",  # sets the timestamp to current time
             )
             embed.set_footer(text="Rehkbot")
-
             embed.set_image(url=banner)
+            file = File(
+                "./assets/images/league_of_legends_sm.png",
+                name="league_of_legends_sm.png",
+            )
+            embed.set_thumbnail(url="attachment://league_of_legends_sm.png")
 
-            hook.send(embed=embed)
+            hook.send(embed=embed, file=file)
+
+            f = open(saved_json, "w")
+            print(json.dumps(responseJSON), file=f)
+
+        f.close()
+
+    async def tftupdates(self):
+        await self.bot.wait_until_ready()
+
+        # patch-notes channel
+        saved_json = "tft_patch_old.json"
+        responseJSON = getTFTGameUpdates()
+
+        # JSON Results Mapping
+        banner = responseJSON["data"]["segments"][0]["thumbnail"]
+        title = responseJSON["data"]["segments"][0]["title"]
+        description = responseJSON["data"]["segments"][0]["description"]
+        url = responseJSON["data"]["segments"][0]["url_path"]
+        tag = responseJSON["data"]["segments"][0]["tag"]
+
+        # check if file exists
+        exists(saved_json)
+
+        time.sleep(5)
+
+        # open saved_json file
+        f = open(
+            saved_json,
+        )
+        data = json.load(f)
+        res = updater(data, "", None)
+        check_file_json = res["data"]["segments"][0]["title"]
+
+        # compare title string from file to title string from api then overwrite file
+        if tag != "teamfight_tactics_patch_notes":
+            print("not patch notes")
+            return
+        elif tag == "teamfight_tactics_patch_notes":
+            # print("False")
+            hook = Webhook(patches_webhook)
+
+            embed = Embed(
+                title="Teamfight Tactics",
+                description=f"[{title}]({url})\n\n{description}",
+                color=crimson,
+                timestamp="now",  # sets the timestamp to current time
+            )
+            embed.set_footer(text="Rehkbot")
+            embed.set_image(url=banner)
+            file = File("./assets/images/tft_logo.png", name="tft_logo.png")
+            embed.set_thumbnail(url="attachment://tft_logo.png")
+
+            # if tag == "teamfight_tactics_patch_notes":
+            hook.send(embed=embed, file=file)
+            # else:
+            #     f = open(saved_json, "w")
+            #     print(json.dumps(responseJSON), file=f)
 
             f = open(saved_json, "w")
             print(json.dumps(responseJSON), file=f)
@@ -166,6 +232,7 @@ class Game_Updates(commands.Cog, name="Game Updates"):
         # add job for scheduler
         scheduler.add_job(self.valupdates, "interval", seconds=3600)
         scheduler.add_job(self.lolupdates, "interval", seconds=3700)
+        scheduler.add_job(self.tftupdates, "interval", seconds=3800)
 
         # starting the scheduler
         scheduler.start()
