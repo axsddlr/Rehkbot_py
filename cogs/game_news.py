5ee39d7088a284fd096227105e94d10acb53413c
Andre Saddler <dresadd09691@gmail.com>
2021-07-08 19:08:04 -0400

    game_news: update to v2 of matches/results

--- cogs/game_news.py
+++ cogs/game_news.py
@@ -30,7 +30,7 @@ def getVLRUpdates():
 
 
 def getVLRMatches():
-    URL = "https://api.axsddlr.xyz/v1/vlr/match/results"
+    URL = "https://api.axsddlr.xyz/v2/vlr/match/results"
     response = requests.get(URL)
     return response.json()
 
@@ -49,43 +49,6 @@ class Game_News(commands.Cog, name="Game News"):
     def __init__(self, bot):
         self.bot = bot
         self.scheduler = AsyncIOScheduler(job_defaults={"misfire_grace_time": 900})
-
-    # async def spike_monitor(self):
-    #     await self.bot.wait_until_ready()
-
-    #     saved_json = "spike_old.json"
-
-    #     # call API
-    #     responseJSON = getSpikeUpdates()
-
-    #     title = responseJSON["today"][0]["title"]
-    #     url = responseJSON["today"][0]["url_path"]
-    #     full_url = "https://thespike.gg" + url
-
-    #     # check if file exists
-    #     exists(saved_json)
-
-    #     time.sleep(5)
-    #     # open saved_json and check title string
-    #     f = open(
-    #         saved_json,
-    #     )
-    #     data = json.load(f)
-    #     res = updater(data, "", None)
-    #     check_file_json = res["today"][0]["title"]
-
-    #     # compare title string from file to title string from api then overwrite file
-    #     if check_file_json == title:
-    #         # print("True")
-    #         return
-    #     elif check_file_json != title:
-    #         # print("False")
-    #         hook = Webhook(spike_webhook)
-    #         hook.send(full_url)
-    #         f = open(saved_json, "w")
-    #         print(json.dumps(responseJSON), file=f)
-
-    #     f.close()
 
     async def vlr_news_monitor(self):
         await self.bot.wait_until_ready()
@@ -130,22 +93,23 @@ class Game_News(commands.Cog, name="Game News"):
     async def vlr_matches_monitor(self):
         await self.bot.wait_until_ready()
 
-        saved_json = "./vlr_matches.json"
+        saved_json = "vlr_matches.json"
 
         # call API
         responseJSON = getVLRMatches()
 
-        team1 = responseJSON["data"]["segments"][0]["team1"]
-        team2 = responseJSON["data"]["segments"][0]["team2"]
-        score1 = responseJSON["data"]["segments"][0]["score1"]
-        score2 = responseJSON["data"]["segments"][0]["score2"]
-        flag1 = responseJSON["data"]["segments"][0]["flag1"]
-        flag2 = responseJSON["data"]["segments"][0]["flag2"]
-        time_completed = responseJSON["data"]["segments"][0]["time_completed"]
-        round_info = responseJSON["data"]["segments"][0]["round_info"]
-        tournament_name = responseJSON["data"]["segments"][0]["tournament_name"]
-        url = responseJSON["data"]["segments"][0]["match_page"]
-        tournament_icon = responseJSON["data"]["segments"][0]["tournament_icon"]
+        team1 = responseJSON["data"][0]["team1"]["name"]
+        team2 = responseJSON["data"][0]["team2"]["name"]
+        score1 = responseJSON["data"][0]["team1"]["score"]
+        score2 = responseJSON["data"][0]["team2"]["score"]
+        flag1 = responseJSON["data"][0]["team1"]["flag"]
+        flag2 = responseJSON["data"][0]["team2"]["flag"]
+        ids = responseJSON["data"][0]["id"]
+        time_completed = responseJSON["data"][0]["status"]
+        round_info = responseJSON["data"][0]["event"]["stage"]
+        tournament_name = responseJSON["data"][0]["event"]["name"]
+        url = responseJSON["data"][0]["link"]
+        tournament_icon = responseJSON["data"][0]["event"]["icon"]
         full_url = "https://www.vlr.gg" + url
 
         # check if file exists
@@ -159,7 +123,7 @@ class Game_News(commands.Cog, name="Game News"):
         data = json.load(f)
         res = updater(data, "", None)
 
-        check_file_json = res["data"]["segments"][0]["team1"]
+        check_file_json = res["data"][0]["team1"]["name"]
 
         # compare title string from file to title string from api then overwrite file
         if check_file_json == team1:
@@ -200,7 +164,7 @@ class Game_News(commands.Cog, name="Game News"):
 
         # add job for scheduler
         # scheduler.add_job(self.spike_monitor, "interval", seconds=1800)
-        scheduler.add_job(self.vlr_matches_monitor, "interval", seconds=180)
+        scheduler.add_job(self.vlr_matches_monitor, "interval", seconds=45)
         scheduler.add_job(self.vlr_news_monitor, "interval", seconds=1800)
 
         # starting the scheduler
